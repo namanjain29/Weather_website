affa0c7e600386489009d0999c2ba5926a37f5e2
namanjain29 <namanjain2912@gmail.com>
2020-04-25 21:29:09 +0530

    Setup app for Heroku

--- public/js/app.js
+++ public/js/app.js
@@ -33,7 +33,7 @@ func = function() {
     }
 }
 
-fetch('http://localhost:3000/weather?address='+ location).then((response)=>{
+fetch('/weather?address='+ location).then((response)=>{
     response.json().then((data)=>{
         val = func()
         if (data.error){
